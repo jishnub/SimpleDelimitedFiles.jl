a12a3c820a7fe0ca98b6cafc70f8ec476366de10
jishnub <jishnub@users.noreply.github.com>
2021-12-28 20:05:02 +0400

    deploydocs from main

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,24 @@
 using SimpleDelimitedFiles
 using Documenter
 
-DocMeta.setdocmeta!(SimpleDelimitedFiles, :DocTestSetup, :(using SimpleDelimitedFiles); recursive=true)
+DocMeta.setdocmeta!(SimpleDelimitedFiles, :DocTestSetup, :(using SimpleDelimitedFiles); recursive = true)
 
 makedocs(;
-    modules=[SimpleDelimitedFiles],
-    authors="Jishnu Bhattacharya",
-    repo="https://github.com/jishnub/SimpleDelimitedFiles.jl/blob/{commit}{path}#L{line}",
-    sitename="SimpleDelimitedFiles",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://jishnub.github.io/SimpleDelimitedFiles.jl",
-        assets=String[],
+    modules = [SimpleDelimitedFiles],
+    authors = "Jishnu Bhattacharya",
+    repo = "https://github.com/jishnub/SimpleDelimitedFiles.jl/blob/{commit}{path}#L{line}",
+    sitename = "SimpleDelimitedFiles",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://jishnub.github.io/SimpleDelimitedFiles.jl",
+        assets = String[]
     ),
-    pages=[
+    pages = [
         "Reference" => "index.md",
-    ],
+    ]
 )
 
 deploydocs(;
-    repo="github.com/jishnub/SimpleDelimitedFiles.jl",
+    repo = "github.com/jishnub/SimpleDelimitedFiles.jl",
+    devbranch = "main",
 )
