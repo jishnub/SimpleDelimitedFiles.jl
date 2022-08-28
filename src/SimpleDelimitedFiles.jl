7385f8c602f1fc89bb50e20a6a14c8ea8bc6d705
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-08-28 11:50:22 +0400

    support headers and comments

--- src/SimpleDelimitedFiles.jl
+++ src/SimpleDelimitedFiles.jl
@@ -1,13 +1,17 @@
 module SimpleDelimitedFiles
 
+using InvertedIndices
+
 @doc raw"""
-    readdlm(source, [delim::Char = '\t'], [T::Type = Float64])
+    readdlm(source, [delim::Char = '\t'], [T::Type = Float64]; [kwargs...])
 
 Read a delimited numerical matrix from the file named `source`. The elements
 of the matrix should be of type `T`. The end of line delimiter is taken as `\n`.
+Unlike `DelimitedFiles`, this package won't try to determine the element type from
+the data, and will throw an error if the values can't be converted to type `T`.
 
-For simple use cases, this is a more performant equivalent of `readdlm`
-from `DelimitedFiles`. There are subtle differences though, for example this package
+For simple use cases, this is a more performant equivalent of `DelimitedFiles.readdlm`.
+There are subtle differences though, for example this package
 tries to aggressively remove multiple delimiters between entries, whereas `DelimitedFiles`
 does not do this.
 
@@ -38,10 +42,35 @@ julia> SimpleDelimitedFiles.readdlm(f, ',', Int)
  1  1
  1  1
 ```
+
+# Optional keyword arguments
+* `skipstart[ = 0]`: Skip the first `skipstart` lines while reading the file
+* `header[ = false]`: Indicates whether there is a row of column headers in the data.
+    This headers may be obtained using [`SimpleDelimitedFiles.read_header`](@ref). This is taken to be
+    `false` by default, which means an error will be thrown if the data does contain a header.
+    If `skipstart` is true, the first `skipstart` lines are ignored before checking for the header.
+* `comments[ = false]`: Indicate whether there are comments in the file that are to be ignored.
+    The comments must begin with `comment_char`.
+* `comment_char[ = '#']`: The character that signifies a comment. If `comments` is `true`, the lines
+    that begin with `comment_char` are ignored.
 """
-function readdlm(fname, delim::Char = '\t', T::Type = Float64)
+function readdlm(fname, delim::Char = '\t', T::Type = Float64;
+        header = false,
+        skipstart = 0, comments=false, comment_char='#')
+
     lines = open(fname, "r") do f
-        readlines(f)
+        lines = readlines(f)
+        lines = @view lines[skipstart + begin:end]
+        lines = lines[Not(findall(isempty, lines))]
+    end
+    if header
+        @info("skipping header, "*
+            "use SimpleDelimitedFiles.read_header(\""*string(fname)*
+            "\") to obtain the columns headers as a vector")
+        lines = @view lines[2:end]
+    end
+    if comments
+        lines = @view lines[Not(findall(startswith(comment_char), lines))]
     end
     nrows = length(lines)
     r = Regex("[$delim]+")
@@ -54,5 +83,35 @@ function readdlm(fname, delim::Char = '\t', T::Type = Float64)
     return M
 end
 readdlm(fname, T::Type) = readdlm(fname, '\t', T)
+
+@doc raw"""
+    read_header(source, [delim::Char = '\t']; [kwargs...])
+
+Read column headers from a file that are separated by `delim`.
+Returns `nothing` if no headers are found in the file.
+
+# Optional keyword arguments
+* `skipstart[ = 0]`: Skip the first `skipstart` lines while reading the file
+* `comments[ = false]`: Indicate whether there are comments in the file that are to be ignored.
+    The comments must begin with `comment_char`.
+* `comment_char[ = '#']`: The character that signifies a comment. If `comments` is `true`, the lines
+    that begin with `comment_char` are ignored.
+"""
+function read_header(fname, delim::Char = '\t';
+    skipstart = 0, comments=false, comment_char='#')
+
+    headerline = open(fname, "r") do f
+        for (ind, line) in enumerate(eachline(f))
+            ind <= skipstart && continue
+            comments && startswith(line, comment_char) && continue
+            return line
+        end
+        return nothing
+    end
+    headerline === nothing && return nothing
+    r = Regex("[$delim]+")
+    ncols = count(r, strip(headerline)) + 1
+    tokens = strip.(split(headerline, delim, keepempty = false))
+end
 
 end
